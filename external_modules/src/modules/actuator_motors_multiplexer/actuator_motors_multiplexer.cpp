2edf03757e7b318fb396ec4e9efa76e9672de714
Jonas Eschmann <jonas.eschmann@gmail.com>
2023-12-12 16:50:14 -0500

    pub sub muxing of actuator outputs works (requires second publisher)

---
+++ external_modules/src/modules/actuator_motors_multiplexer/actuator_motors_multiplexer.cpp
@@ -0,0 +1,150 @@
+#include "actuator_motors_multiplexer.hpp"
+
+ActuatorMotorsMultiplexer::ActuatorMotorsMultiplexer() : ModuleParams(nullptr), ScheduledWorkItem(MODULE_NAME, px4::wq_configurations::rate_ctrl) {
+	_actuator_motors_mux_pub.advertise();
+}
+
+ActuatorMotorsMultiplexer::~ActuatorMotorsMultiplexer()
+{
+	perf_free(_loop_perf);
+	perf_free(_loop_interval_perf);
+}
+
+bool ActuatorMotorsMultiplexer::init()
+{
+	ScheduleOnInterval(2000_us); // 2000 us interval, 200 Hz rate
+	this->init_time = hrt_absolute_time();
+
+	if (!_actuator_motors_sub.registerCallback()) {
+		PX4_ERR("actuator_motors_sub callback registration failed");
+		return false;
+	}
+	if (!_actuator_motors_rl_tools_sub.registerCallback()) {
+		PX4_ERR("actuator_motors_rl_tools_sub callback registration failed");
+		return false;
+	}
+	return true;
+}
+
+void ActuatorMotorsMultiplexer::Run()
+{
+	if (should_exit()) {
+		_actuator_motors_sub.unregisterCallback();
+		_actuator_motors_rl_tools_sub.unregisterCallback();
+		ScheduleClear();
+		exit_and_cleanup();
+		return;
+	}
+
+	uint32_t current_time = hrt_absolute_time();
+
+	bool prev_use_original_controller = this->use_original_controller;
+	bool next_use_original_controller = this->use_original_controller;
+
+	{
+		manual_control_setpoint_s manual_control_input;
+		if(_manual_control_input_sub.update(&manual_control_input)) {
+			last_rc_update_time_set = true;
+			last_rc_update_time = current_time;
+			next_use_original_controller = manual_control_input.aux1 < 0.5f;
+			PX4_INFO("manual_control_input.aux1: %f", (double)manual_control_input.aux1);
+		}
+	}
+
+
+	constexpr uint32_t RL_TOOLS_CONTROLLER_TIMEOUT = 100*1000; // 100ms timeout
+	constexpr uint32_t RC_TRIGGER_TIMEOUT = 200*1000; // 200ms timeout
+	next_use_original_controller = next_use_original_controller || !last_rl_tools_output_time_set || !last_rc_update_time_set;
+	if(last_rc_update_time_set && ((current_time - last_rc_update_time) > RC_TRIGGER_TIMEOUT)){
+		next_use_original_controller = true;
+	}
+	if(last_rl_tools_output_time_set && ((current_time - last_rl_tools_output_time) > RL_TOOLS_CONTROLLER_TIMEOUT)){
+		next_use_original_controller = true;
+	}
+	if(prev_use_original_controller != next_use_original_controller){
+		if(next_use_original_controller){
+			PX4_INFO("Switching to original controller");
+		}
+		else{
+			PX4_INFO("Switching to RLtools controller");
+		}
+	}
+
+	this->use_original_controller = next_use_original_controller;
+
+	actuator_motors_s actuator_motors;
+	if(this->use_original_controller){
+		if(_actuator_motors_sub.update(&actuator_motors)){
+			_actuator_motors_mux_pub.publish(actuator_motors);
+		}
+	}
+	else{
+		if(_actuator_motors_rl_tools_sub.update(&actuator_motors)){
+			last_rl_tools_output_time_set = true;
+			last_rl_tools_output_time = current_time;
+			_actuator_motors_mux_pub.publish(actuator_motors);
+		}
+	}
+	perf_count(_loop_interval_perf);
+
+}
+
+int ActuatorMotorsMultiplexer::task_spawn(int argc, char *argv[])
+{
+	ActuatorMotorsMultiplexer *instance = new ActuatorMotorsMultiplexer();
+
+	if (instance) {
+		_object.store(instance);
+		_task_id = task_id_is_work_queue;
+
+		if (instance->init()) {
+			return PX4_OK;
+		}
+
+	} else {
+		PX4_ERR("alloc failed");
+	}
+
+	delete instance;
+	_object.store(nullptr);
+	_task_id = -1;
+
+	return PX4_ERROR;
+}
+
+int ActuatorMotorsMultiplexer::print_status()
+{
+	perf_print_counter(_loop_perf);
+	perf_print_counter(_loop_interval_perf);
+	return 0;
+}
+
+int ActuatorMotorsMultiplexer::custom_command(int argc, char *argv[])
+{
+	return print_usage("unknown command");
+}
+
+int ActuatorMotorsMultiplexer::print_usage(const char *reason)
+{
+	if (reason) {
+		PX4_WARN("%s\n", reason);
+	}
+
+	PRINT_MODULE_DESCRIPTION(
+		R"DESCR_STR(
+### Description
+RLtools Benchmark
+
+)DESCR_STR");
+
+	PRINT_MODULE_USAGE_NAME("rl_tools_benchmark", "template");
+	PRINT_MODULE_USAGE_COMMAND("start");
+	PRINT_MODULE_USAGE_DEFAULT_COMMANDS();
+
+	return 0;
+}
+
+extern "C" __EXPORT int actuator_motors_multiplexer_main(int argc, char *argv[])
+{
+	return ActuatorMotorsMultiplexer::main(argc, argv);
+}
---
+++ external_modules/src/modules/actuator_motors_multiplexer/actuator_motors_multiplexer.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+
+#include <px4_platform_common/defines.h>
+#include <px4_platform_common/module.h>
+#include <px4_platform_common/module_params.h>
+#include <px4_platform_common/posix.h>
+#include <px4_platform_common/px4_work_queue/ScheduledWorkItem.hpp>
+
+#include <drivers/drv_hrt.h>
+#include <lib/perf/perf_counter.h>
+
+#include <uORB/Publication.hpp>
+#include <uORB/Subscription.hpp>
+#include <uORB/SubscriptionCallback.hpp>
+#include <uORB/topics/actuator_motors.h>
+#include <uORB/topics/manual_control_setpoint.h>
+
+using namespace time_literals;
+
+class ActuatorMotorsMultiplexer : public ModuleBase<ActuatorMotorsMultiplexer>, public ModuleParams, public px4::ScheduledWorkItem
+{
+public:
+	ActuatorMotorsMultiplexer();
+	~ActuatorMotorsMultiplexer() override;
+
+	/** @see ModuleBase */
+	static int task_spawn(int argc, char *argv[]);
+
+	/** @see ModuleBase */
+	static int custom_command(int argc, char *argv[]);
+
+	/** @see ModuleBase */
+	static int print_usage(const char *reason = nullptr);
+
+	bool init();
+
+	int print_status() override;
+
+private:
+	void Run() override;
+	uint32_t init_time, last_rl_tools_output_time, last_rc_update_time;
+	bool last_rl_tools_output_time_set = false, last_rc_update_time_set = false;
+	uORB::Subscription _manual_control_input_sub{ORB_ID(manual_control_input)};
+	uORB::SubscriptionCallbackWorkItem _actuator_motors_sub{this, ORB_ID(actuator_motors)};
+	uORB::SubscriptionCallbackWorkItem _actuator_motors_rl_tools_sub{this, ORB_ID(actuator_motors_rl_tools)};
+	uORB::Publication<actuator_motors_s> _actuator_motors_mux_pub{ORB_ID(actuator_motors_mux)};
+	bool use_original_controller = true;
+
+	// Performance (perf) counters
+	perf_counter_t	_loop_perf{perf_alloc(PC_ELAPSED, MODULE_NAME": cycle")};
+	perf_counter_t	_loop_interval_perf{perf_alloc(PC_INTERVAL, MODULE_NAME": interval")};
+
+};
