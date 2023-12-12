b567c6fa00c003a7142f681bb2aaa6eb98a84828
Jonas Eschmann <jonas.eschmann@gmail.com>
2023-12-12 18:14:29 -0500

    switching seems to work well

--- external_modules/src/modules/actuator_motors_multiplexer/actuator_motors_multiplexer.cpp
+++ external_modules/src/modules/actuator_motors_multiplexer/actuator_motors_multiplexer.cpp
@@ -52,7 +52,7 @@ void ActuatorMotorsMultiplexer::Run()
 
 	constexpr uint32_t RL_TOOLS_CONTROLLER_TIMEOUT = 100*1000; // 100ms timeout
 	constexpr uint32_t RC_TRIGGER_TIMEOUT = 2000*1000; // 200ms timeout
-	// next_use_original_controller = next_use_original_controller || !last_rl_tools_output_time_set || !last_rc_update_time_set;
+	next_use_original_controller = next_use_original_controller || !last_rl_tools_output_time_set || !last_rc_update_time_set;
 	if(last_rc_update_time_set && ((current_time - last_rc_update_time) > RC_TRIGGER_TIMEOUT)){
 		next_use_original_controller = true;
 	}
@@ -70,17 +70,24 @@ void ActuatorMotorsMultiplexer::Run()
 
 	this->use_original_controller = next_use_original_controller;
 
-	actuator_motors_s actuator_motors;
+	actuator_motors_s actuator_motors, actuator_motors_rl_tools;
+	bool actuator_motors_set = false, actuator_motors_rl_tools_set = false;
+	if(_actuator_motors_sub.update(&actuator_motors)){
+		actuator_motors_set = true;
+	}
+	if(_actuator_motors_rl_tools_sub.update(&actuator_motors_rl_tools)){
+		last_rl_tools_output_time_set = true;
+		last_rl_tools_output_time = current_time;
+		actuator_motors_rl_tools_set = true;
+	}
 	if(this->use_original_controller){
-		if(_actuator_motors_sub.update(&actuator_motors)){
+		if(actuator_motors_set){
 			_actuator_motors_mux_pub.publish(actuator_motors);
 		}
 	}
 	else{
-		if(_actuator_motors_rl_tools_sub.update(&actuator_motors)){
-			last_rl_tools_output_time_set = true;
-			last_rl_tools_output_time = current_time;
-			_actuator_motors_mux_pub.publish(actuator_motors);
+		if(actuator_motors_rl_tools_set){
+			_actuator_motors_mux_pub.publish(actuator_motors_rl_tools);
 		}
 	}
 	perf_count(_loop_interval_perf);
