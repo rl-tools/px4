d8745de0e074f670b6260f464c825afa6a9e229e
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-01-09 17:22:02 +0400

    fixed x500 geom

--- sysid/geometry/x500.py
+++ sysid/geometry/x500.py
@@ -2,8 +2,8 @@ import numpy as np
 
 gravity = 9.81
 mass = 2
-rotor_x_displacement = 0.25
-rotor_y_displacement = 0.25
+rotor_x_displacement = 0.1765
+rotor_y_displacement = 0.1765
 rotor_positions = np.array([
     [ rotor_x_displacement, -rotor_y_displacement, 0],
     [-rotor_x_displacement,  rotor_y_displacement, 0],
